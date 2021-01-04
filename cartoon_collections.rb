56149894df1423074a091e617bb579627b48aaa3
Michael Rabe <m.trabe96@gmail.com>
2021-01-04 01:02:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,5 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find {|element| element 
+  planeteer_calls.find {|element| element == valid_calls}
 end
